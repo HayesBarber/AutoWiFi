afb4f5feb5a8d99924e07ea90c5a4e465d386035
Hayes Barber <barberhayes@gmail.com>
2025-06-30 21:19:43 -0400

    stub methods

--- src/AutoWiFi.h
+++ src/AutoWiFi.h
@@ -1,9 +1,15 @@
 #ifndef AUTOWIFI_H
 #define AUTOWIFI_H
 
+#include <WiFi.h>
+
 class AutoWiFi {
 public:
     AutoWiFi();
+    void connect();
+    void update();
+    bool isConnected() const;
+    IPAddress getIP() const;
 };
 
 #endif
